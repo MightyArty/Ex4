e330cac2ebd1ba18d486ecb26c67c971d9055548
liorpatael <Lior13313>
2022-01-05 12:32:08 +0200

    our algo

--- src/client_python/Pokemon.py
+++ src/client_python/Pokemon.py
@@ -10,6 +10,12 @@ class Pokemon:
     def get_pos(self):
         return self.pos
 
+    def getX(self):
+        return self.pos[0]
+
+    def getY(self):
+        return self.pos[1]
+
     def isOn(self, src_X, src_Y, dest_X, dest_Y) -> bool:
         m = float((src_Y - dest_Y) / (src_X - dest_X))
         Y = self.pos[1] - src_Y
